df040ae13a9ad152964fcc56e82f07801a261ee8
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-25 19:55:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,6 @@ end
 def add_element_to_end_of_array(array, element)
   cafe_drinks = ["coffee", "tea", "espresso", "frap"]
   cafe_drinks << "latte"
-  return cafe_drinks
 end
 
 def add_element_to_start_of_array(array, element)
