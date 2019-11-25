3d06ecdd1c45291195532a5f0e4b746555821e2f
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-25 20:10:18 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,44 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ cafe_drinks = ["coffee", "tea", "espresso", "frap"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  cafe_drinks = ["coffee", "tea", "espresso", "frap"]
+  cafe_drinks.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  cafe_drinks = ["coffee", "tea", "espresso", "frap"]
+  cafe_drinks.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  cafe_drinks = ["coffee", "tea", "espresso", "frap"]
+  frap_drink = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  cafe_drinks = ["coffee", "tea", "espresso", "frap"]
+  coffe_drink = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  cafe_drinks = ["coffee", "tea", "espresso", "frap"]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+array[index_number] = element
 end
